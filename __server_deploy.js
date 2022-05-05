0960cbac2837bdc02de3cc97b6293df1c448f2f5
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-05-05 16:55:36 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,16 @@
+/**
+ * --------------------------
+ * ONE TIME for your Computer
+ * --------------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ * 
+ * -------------------------
+ * For each project One time
+ * -------------------------
+ * 1. heroku create
+ * 2. 
+ * 
+*/
